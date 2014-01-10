f34b41225baaa6371c28b11c7af4c0b6acaa4377
Loïc Bouilly <loic.bouilly@gmail.com>
2014-01-10 17:06:05 +0100

    Mise en page 3

--- src/main.py
+++ src/main.py
@@ -75,7 +75,8 @@ class Socket(object):
                 print Colors.OKGREEN + ('Got initial value: %s %s' % (self.__last_value, self.__currency)) + Colors.ENDC
 
                 self.__lcd.clear()
-                self.__lcd.message(str(self.__last_value))
+                self.__lcd.message("%s Value : \n" % (self.__currency))
+                self.__lcd.message(str(self.__last_value) + ' %s \n' %(self.__currency_current))
 
         self.open_socket()
 
